b4972fc6bb8bc4ad1de569d5da07eaad2a5cd71a
Emre <130907164+monsieuremre@users.noreply.github.com>
2023-04-16 12:41:27 +0000

    add project link to license notice

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-// This file is part of RAETSELx86 <>
+// This file is part of RAETSELx86 <https://github.com/monsieuremre/raetselx86>
 // RAETSELx86 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 // License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
 // version. RAETSELx86 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
